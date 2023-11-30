9d93c196bd9339367a15b379cba1176b81338132
europa <myiron11@gmail.com>
2023-11-30 08:28:52 +0000

    Home 구현

--- src/App.js
+++ src/App.js
@@ -1,170 +1,101 @@
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useReducer,
-  useRef,
-  createContext,
-} from "react";
-import DiaryEditor from "./DiaryEditor";
-import DiaryList from "./DiaryList";
-import "./App2.css";
+import React, { useRef, useReducer } from "react";
+
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.js";
 import Edit from "./pages/Edit.js";
 import New from "./pages/New.js";
 import Diary from "./pages/Diary.js";
-import MyHeader from "./components/MyHeader.js";
-import MyButton from "./components/MyButton.js";
-export const DiaryStateContext = createContext(null);
-export const DiaryDispatchContext = createContext(null);
+
+export const DiaryStateContext = React.createContext(null);
+export const DiaryDispatchContext = React.createContext(null);
+const dummyData = [
+  { id: 1, emotion: 1, content: "오늘의일기 1번", date: 1701327309982 },
+  { id: 2, emotion: 2, content: "오늘의일기 2번", date: 1701327309983 },
+  { id: 3, emotion: 3, content: "오늘의일기 3번", date: 1701327309984 },
+  { id: 4, emotion: 4, content: "오늘의일기 4번", date: 1701327309985 },
+  { id: 5, emotion: 5, content: "오늘의일기 5번", date: 1701327309986 },
+];
 
 const reducer = (state, action) => {
+  let newState = [];
   switch (action.type) {
     case "INIT": {
       return action.data;
     }
     case "CREATE": {
-      const created_date = new Date().getTime();
       const newItem = {
         ...action.data,
-        created_date,
       };
-      return [newItem, ...state];
+      newState = [newItem, ...state];
+      break;
     }
     case "REMOVE": {
-      return state.filter((it) => it.id !== action.targetId);
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
     }
     case "EDIT": {
-      return state.map((it) =>
-        it.id === action.targetId
-          ? {
-              ...it,
-              content: action.newContent,
-            }
-          : it
+      newState = state.map((it) =>
+        it.id === action.targetId ? { ...action.data } : it
       );
+      break;
     }
     default:
       return state;
   }
+
+  return newState;
 };
-
 const App = () => {
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, dummyData);
+
   const dataId = useRef(0);
-  const getData = async () => {
-    setTimeout(async () => {
-      const res = await fetch(
-        "https://jsonplaceholder.typicode.com/comments"
-      ).then((res) => res.json());
-
-      const initData = res.slice(0, 20).map((it) => {
-        return {
-          author: it.email,
-          content: it.body,
-          emotion: Math.floor(Math.random() * 5) + 1,
-          created_date: new Date().getTime(),
-          id: dataId.current++,
-        };
-      });
-
-      dispatch({ type: "INIT", data: initData });
-    }, 2000);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const onCreate = useCallback((author, content, emotion) => {
+  // CREATE
+  const onCreate = (date, content, emotion) => {
     dispatch({
       type: "CREATE",
-      data: { author, content, emotion, id: dataId.current },
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
     });
     dataId.current += 1;
-  }, []);
-
-  const onRemove = useCallback((targetId) => {
+  };
+  // REMOVE
+  const onRemove = (targetId) => {
     dispatch({ type: "REMOVE", targetId });
-  }, []);
+  };
 
-  const onEdit = useCallback((targetId, newContent) => {
+  const onEdit = (targetId, date, content, emotion) => {
     dispatch({
       type: "EDIT",
-      targetId,
-      newContent,
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
     });
-  }, []);
+  };
 
-  const memoizedDispatch = useMemo(() => {
-    return { onCreate, onRemove, onEdit };
-  }, []);
-
-  const getDiaryAnalysis = useMemo(() => {
-    const goodCount = data.filter((it) => it.emotion >= 3).length;
-    const badCount = data.length - goodCount;
-    const goodRatio = (goodCount / data.length) * 100.0;
-    return { goodCount, badCount, goodRatio };
-  }, [data.length]);
-
-  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
-  /*
+  // EDIT
   return (
     <DiaryStateContext.Provider value={data}>
-      <DiaryDispatchContext.Provider value={memoizedDispatch}>
-        <div className="App">
-          <DiaryEditor />
-          <div>전체 일기 : {data.length}</div>
-          <div>기분 좋은 일기 개수 : {goodCount}</div>
-          <div>기분 나쁜 일기 개수 : {badCount}</div>
-          <div>기분 좋은 일기 비율 : {goodRatio}%</div>
-          <DiaryList />
-        </div>
+      <DiaryDispatchContext.Provider value={(onCreate, onRemove, onEdit)}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
       </DiaryDispatchContext.Provider>
     </DiaryStateContext.Provider>
-  );
-*/
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader
-          headText="App"
-          leftChild={
-            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽클릭")} />
-          }
-          rightChild={
-            <MyButton
-              text={"오른쪽 버튼"}
-              onClick={() => alert("오른쪽 클릭")}
-            />
-          }
-        />
-        <h2>App.js</h2>
-        <img src={process.env.PUBLIC_URL + "/assets/emotion1.png"}></img>
-        <img src={process.env.PUBLIC_URL + "/assets/emotion2.png"}></img>
-        <img src={process.env.PUBLIC_URL + "/assets/emotion3.png"}></img>
-        <img src={process.env.PUBLIC_URL + "/assets/emotion4.png"}></img>
-        <img src={process.env.PUBLIC_URL + "/assets/emotion5.png"}></img>
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼클릭")}
-          type={"positive"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼클릭")}
-          type={"negative"}
-        />
-        <MyButton text={"버튼"} onClick={() => alert("버튼클릭")} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/diary/:id" element={<Diary />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,57 @@
+import { useState, useEffect, useContext } from "react";
+import MyHeader from "./../components/MyHeader";
+import MyButton from "./../components/MyButton";
+import DiaryList from "./../components/DiaryList";
+import { DiaryStateContext } from "./../App";
+
 const Home = () => {
+  const [curDate, setCurDate] = useState(new Date());
+  const diaryList = useContext(DiaryStateContext);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime();
+
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        1
+      ).getTime();
+
+      setData(
+        diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay)
+      );
+    }
+  }, [diaryList, curDate]);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
   return (
     <div>
-      <h1>Home</h1>
-      <p>이 곳은 홈 입니다.</p>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      />
+      <DiaryList diaryList={diaryList} />
     </div>
   );
 };
